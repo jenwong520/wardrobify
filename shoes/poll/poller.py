06ece61412d4314667903da6b0b52c86a8d216e9
Jennifer Wong <jmw520@gmail.com>
2024-08-09 09:25:00 -0700

    Made revisions to README

--- shoes/poll/poller.py
+++ shoes/poll/poller.py
@@ -34,7 +34,7 @@ def poll():
             get_bins()
         except Exception as e:
             print(e, file=sys.stderr)
-        time.sleep(5)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
